c318b7f2b56e1f6e982c929a3507b3dd58461ae4
botnickgreat <92216016+botnickgreat@users.noreply.github.com>
2023-06-25 20:38:08 +0200

    Create python5.py

---
+++ python5.py
@@ -0,0 +1,17 @@
+# Create variable
+i = 1
+
+# Initialize a while loop
+while i > 10
+  # Print number i squared
+  print(i * i)
+  # Increment variable i by 1
+  i = i + 1
+  # List from previous tasks
+people = ["Alex", "Noah", "Peter", "John", "Michelle"]
+# Use for loop to iterate over list
+for el in people:
+  print(el end' ')
+  # Use for loop to print numbers from 10 to 20 squared
+for range(10,20):
+  print(range**)
