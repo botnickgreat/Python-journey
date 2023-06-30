f4bd861ed9628f4aba10145331332bacc0731de1
botnickgreat <92216016+botnickgreat@users.noreply.github.com>
2023-06-30 17:09:17 +0200

    Create pythonlastday.py

---
+++ pythonlastday.py
@@ -0,0 +1,70 @@
+statement1 = 42 > 1000
+statement2 = 45 < 8
+statement3 = 3 > 6
+
+# Print these stements and check if it False 
+print(statement1)
+print(statement2)
+print(statement3)
+
+# Replace gaps to make these statements True
+statement1 = 0 == 0
+statement2 = 800 >= 800
+statement3 = 2800 <= 2800
+
+# Print these stements and check if it True
+print(statement1)
+print(statement2)
+print(statement3)
+
+# This statement returns True
+statement1 = True and True
+# This statement returns False
+statement2 = False or False
+
+# Print the True statement
+print(statement1)
+# Print the False statement
+print(statement2)
+
+# This statement equals False
+statement1 = True and False
+# This statement equals True
+statement2 = False or True
+
+# Print the False statement
+print(statement1)
+# Print the True statement
+print(statement2)
+
+# This statement equals False
+statement1 = not True or False
+# This statement equals True
+statement2 = True and not False
+
+# Print the first statement
+print(statement1)
+# Print the second statement
+print(statement2)
+
+# This statement equals True
+statement1 = True and not False
+# This statement equals False
+statement2 = not True and True
+
+# Print the first statement
+print(statement1)
+# Print the second statement
+print(statement2)
+
+# Make these statements equal True
+statement1 = True or (True and False)
+statement2 = (False and (True and True)) or True
+statement3 = ((False or False) or True) and True
+
+# Print the first statement
+print(statement1)
+# Print the second statement
+print(statement2)
+# Print the third statement
+print(statement3)
