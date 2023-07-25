4b03d0cd1f4569f5d841394ead451f1e6401ab2a
botnickgreat <92216016+botnickgreat@users.noreply.github.com>
2023-07-25 17:48:54 +0200

    Create pythonday22.py

---
+++ pythonday22.py
@@ -0,0 +1,27 @@
+# Complete the first task
+string1 = "good"
+# Complete the second task
+string2 = "could be better"
+# Complete the third task
+string3 = "no"
+
+print("The weather outside is", string1)
+print("My mood today", string2)
+print("Is my mood depend on weather? -", string3)
+# Put appropriate quotation marks here
+string = "John said, 'I am learning data types in Python now'"
+
+# Print the string
+print(string)
+string1 = "I am learning how to code!"
+string2 = "I am very persistent!"
+
+# Find these letters in the code and get it using relevant index
+I = string1[0]
+l = string1[5]
+o = string1[15]
+v = string2[5]
+e = string1[6]
+y = string2[8]
+
+print(I, "", l, o, v, e, "", y, o, "u")
