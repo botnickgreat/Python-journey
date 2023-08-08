b9197dbc6e5bc6c132767dea2d3807f003731d97
botnickgreat <92216016+botnickgreat@users.noreply.github.com>
2023-08-08 12:03:44 +0200

    Create finishingpython.py

---
+++ finishingpython.py
@@ -0,0 +1,25 @@
+print("Number of vertices of a pyramid is " + str(5))
+
+print("Number of Major Arcana cards in divinatory Tarot is " + str(22))
+
+print("Largest number that is not the sum of distinct squares is " + str(128))
+
+string1 = "Over"
+string2 = "8 million"
+string3 = "people use Python"
+
+# Print the first string twice
+print(string1 * 2 )
+# Print the second string three times
+print(string2 * 3)
+# Print the third string once
+print(string3)
+
+# Make this statement equals False
+print('C' > 'D')
+
+# Make this statement equals False
+print("Codefinity" < "Art")
+
+# Make this statement equals True
+print("Programming" > "Data")
